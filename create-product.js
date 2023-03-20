e6ae088b4692ffc5e6a127eef0c715937d852514
vsiago <vsiago23@gmail.com>
2023-03-20 08:27:30 -0300

    tentando salvar imagem no objeto

--- create-product.js
+++ create-product.js
@@ -1,13 +1,13 @@
 "use strict"
 // Selecionar elementos
 
-const btnCriarProduto = document.querySelector("#btnCriarProduto");
+const btnAbrirModal = document.querySelector("#btnAbrirModal");
 const modalCriarProduto = document.querySelector("#modalCriarProduto");
 const btnClose = document.querySelector("#btnClose");
 
 
 // Abrir modal Criar produto
-btnCriarProduto.addEventListener("click", function openModal() {
+btnAbrirModal.addEventListener("click", function openModal() {
   modalCriarProduto.style.display = "flex";
 });
 
@@ -17,11 +17,11 @@ btnClose.addEventListener("click", function closeModal() {
 });
 
 // Trocar input por imagem
-const inputPhoto = document.querySelector("#inputPhoto")
-const inputFile = document.querySelector("#inputFile")
+const inputPhoto = document.querySelector("#inputPhoto");
+const inputFile = document.querySelector("#inputFile");
 
 inputPhoto.addEventListener("click", () => {
-  inputFile.click()
+  inputFile.click();
 })
 
 
@@ -45,16 +45,56 @@ inputFile.addEventListener("change", () => {
 
 
 
+const produtos = [];
+
+function adicionarProduto(e) {
+  e.preventDefault()
+  
+  const produto = {
+    nome: document.querySelector('#name').value,
+    preco: document.querySelector('#preco').value,
+    descricao: document.querySelector('#descricao').value
+  }
+  
+  
+    produtos.push(produto);
+
+  exibirProdutos()
+}
 
 
 
+const btnCriarProduto = document.querySelector('#btnCriarProduto')
+
+btnCriarProduto.addEventListener('click', adicionarProduto)
 
 
+//Saida de dados
 
 
+function exibirProdutos() {
+  const listaProdutos = document.getElementById("lista-produtos");
+  listaProdutos.innerHTML = ''
+  
 
-
-
+  produtos.forEach(function(produto) {
+    const item = document.createElement("li");
+    item.classList.add("product-card")
+    item.innerHTML = `
+    <img id="productImg" src="./pizza-calabreza.png" alt="Imagem do produto">
+    <div>
+      <p class="product-name">${produto.nome}</p>
+      <p class="price">${produto.preco}</p>
+      <div class="amount-controller">
+        <p class="amount">01</p>
+        <button>+</button>
+        <button>-</button>
+      </div>        
+    </div>
+    `;
+    listaProdutos.appendChild(item);
+  });
+}
 
 
 // const listaDeEstabelecimentos = [
